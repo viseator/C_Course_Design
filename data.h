61b5140f3cbd5a5408e9c1f2bcd1fb53b1882290
viseator <viseator@gmail.com>
2017-08-20 11:30:48 +0800

    add header

--- data.h
+++ data.h
@@ -6,12 +6,13 @@
 #include <string.h>
 #include <ctype.h>
 #include "utils.h"
+
 #define NAME_SIZE 22
 
-FILE* student_file;
-FILE* class_file;
-FILE* grade_file;
-FILE* info_file;
+FILE *student_file;
+FILE *class_file;
+FILE *grade_file;
+FILE *info_file;
 
 typedef struct student {
     char id[12];
@@ -49,4 +50,61 @@ typedef struct grade {
     char chairman[NAME_SIZE];
     List *classes;
 } Grade;
+
+
+List *readGradeFromFile();
+
+List *readClassFromFile(int num);
+
+List *readStudentFromFile(int num);
+
+void openFileForRead();
+
+void saveGradeToFile(List *grade_list);
+
+void saveClassToFile(List *class_list);
+
+void saveStudentToFile(List *student_list);
+
+void writeListToFile(List *list, FILE *file);
+
+void closeFile();
+
+void openFileForWrite();
+
+List *getStudentByAge(List *student_list, int min, int max);
+
+List *getStudentByWhere(List *student_list, const char *where);
+
+List *getStudentByName(List *student_list, const char *name);
+
+List *getClassByNum(List *class_list, int min, int max);
+
+List *getClassByTeaName(List *class_list, const char *name);
+
+List *getClassByName(List *class_list, const char *name);
+
+List *getClassById(List *class_list, const char *id);
+
+List *getGradeByNum(List *grade_list, int min, int max);
+
+List *getGradeByTime(List *grade_list, int min, int max);
+
+List *getGradeById(List *grade_list, const char *id);
+
+Student *getStudent(List *class, int position);
+
+Class *getClass(List *grade, int position);
+
+Grade *getGrade(List *grade_list, int position);
+
+void addStudent(Class *class, char *id, char *name, char *gender, char *hometown, char *birth, char *phone, float score,
+                int age, bool grad, char *where);
+
+void addClass(Grade *grade, char *id, char *name, int num, float age, int gra_num, char *mon_name, char *phone,
+              char *tea_name, char *tea_phone);
+
+void
+addGrade(List *list, char *id, char *time, int num, int gra_num, char *coun_name, char *coun_phone, char *chairman);
+
 #endif
